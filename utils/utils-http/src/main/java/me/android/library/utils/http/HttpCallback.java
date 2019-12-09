b2d563969a001fcbaa622d5a93a689b86e4e2e89
sylar <81623859@qq.com>
2019-12-10 00:36:11 +0800

    init commit

---
+++ utils/utils-http/src/main/java/me/android/library/utils/http/HttpCallback.java
@@ -0,0 +1,21 @@
+package me.android.library.utils.http;
+
+import me.java.library.common.Callback;
+
+/**
+ * File Name             :  HttpCallback
+ *
+ * @author :  sylar
+ * Create                :  2019-12-09
+ * Description           :
+ * Reviewed By           :
+ * Reviewed On           :
+ * Version History       :
+ * Modified By           :
+ * Modified Date         :
+ * Comments              :
+ * CopyRight             : COPYRIGHT(c) allthings.vip  All Rights Reserved
+ * *******************************************************************************************
+ */
+public interface HttpCallback<T> extends Callback<T> {
+}

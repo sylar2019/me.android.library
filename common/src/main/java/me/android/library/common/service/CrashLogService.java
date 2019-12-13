c4eaab371b5a90a9223c7251cdf622151e9a5346
sylar <81623859@qq.com>
2019-12-14 01:38:27 +0800

    init commit

--- common/src/main/java/me/android/library/common/service/CrashLogService.java
+++ common/src/main/java/me/android/library/common/service/CrashLogService.java
@@ -9,16 +9,14 @@ import android.os.Environment;
 
 import com.google.common.collect.Maps;
 
+import org.joda.time.DateTime;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.lang.reflect.Field;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Map;
 
 import me.android.library.common.utils.AppUtils;
@@ -27,11 +25,6 @@ import me.java.library.utils.base.FileUtils;
 
 public class CrashLogService extends AbstractService {
 
-    private final static String PATH = Environment.getExternalStorageDirectory()
-            .getPath().concat(File.separator).concat("_crash")
-            .concat(File.separator);
-    private static DateFormat formatter = new SimpleDateFormat(
-            "yyyy-MM-dd-HH-mm-ss", Locale.getDefault());
     private UncaughtExceptionHandler mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
     private Map<String, String> infos = Maps.newHashMap();
     private OnCrashedListener listener;
@@ -40,27 +33,23 @@ public class CrashLogService extends AbstractService {
         // 收集错误信息 发送错误报告
         // 如果用户没有处理则让系统默认的异常处理器来处理
         // 退出程序
-        UncaughtExceptionHandler errorHandler = new UncaughtExceptionHandler() {
+        UncaughtExceptionHandler errorHandler = (thread, ex) -> {
 
-            @Override
-            public void uncaughtException(Thread thread, Throwable ex) {
+            // 收集错误信息 发送错误报告
+            handleException(ex);
 
-                // 收集错误信息 发送错误报告
-                handleException(ex);
+            // 如果用户没有处理则让系统默认的异常处理器来处理
+            mDefaultHandler.uncaughtException(thread, ex);
 
-                // 如果用户没有处理则让系统默认的异常处理器来处理
-                mDefaultHandler.uncaughtException(thread, ex);
-
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                // 退出程序
-                android.os.Process.killProcess(android.os.Process.myPid());
-                System.exit(1);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+
+            // 退出程序
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
         };
         Thread.setDefaultUncaughtExceptionHandler(errorHandler);
     }
@@ -69,15 +58,6 @@ public class CrashLogService extends AbstractService {
         return CrashLogService.SingletonHolder.instance;
     }
 
-    /**
-     * 测试crashLog
-     */
-//    @SuppressWarnings("null")
-//    public void test() {
-//        String a = null;
-//        assert a != null;
-//        a.concat(null);
-//    }
     public void setOnCrashedListener(OnCrashedListener l) {
         listener = l;
     }
@@ -93,12 +73,9 @@ public class CrashLogService extends AbstractService {
 
         // 收集设备参数信息
         collectDeviceInfo(cx);
-
         // 保存日志文件
         String log = saveCrashInfo2File(ex);
-
         onCrashLog(log);
-
     }
 
     private void collectDeviceInfo(Context ctx) {
@@ -151,14 +128,13 @@ public class CrashLogService extends AbstractService {
         sb.append(result);
 
         try {
-            long timestamp = System.currentTimeMillis();
-            String time = formatter.format(new Date());
-            String fileName = "crash-" + time + "-" + timestamp + ".txt";
-            if (Environment.getExternalStorageState().equals(
-                    Environment.MEDIA_MOUNTED)) {
+            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+            String fileName = String.format("%s%scrash-%s.txt",
+                    path,
+                    File.separator,
+                    new DateTime().toString("yyyy-MM-dd HH:mm:ss"));
+            FileUtils.writeFile(fileName, sb.toString(), false);
 
-                FileUtils.writeFile(PATH + fileName, sb.toString(), false);
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
